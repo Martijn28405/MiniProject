fc47d91332297a6d0d9d30159133e7d81801affc
Luca Stuart <lucastuart@145-137-2-174.wlan.hro.nl>
2023-02-16 16:39:44 +0100

    add

---
+++ Item.cs
@@ -0,0 +1,15 @@
+public class Item
+{
+    public int ID;
+    public string Name;
+    public string NamePlural;
+
+    public Monster(int id, string name, string namePlural)
+    {
+        this.ID = id;
+        this.Name = name;
+        this.NamePlural = namePlural;
+
+
+    }
+}
