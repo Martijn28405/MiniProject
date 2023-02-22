1e7870830342da13ccfd67e365e752b1cf01f652
mjvdnet <mjvdnet@gmail.com>
2023-02-22 13:21:02 +0100

    Small edits to the spider quest text.

--- SpiderQuest.cs
+++ SpiderQuest.cs
@@ -16,7 +16,7 @@ namespace MiniProject
             string userChoice = Console.ReadLine();
             if (userChoice == "1")
             {
-                System.Console.WriteLine("You chose to ignore the adventurers advice!\nYou almost immediately see a horde of spiders, they look stronger than usual.");
+                System.Console.WriteLine("You chose to ignore the adventurers advice!\nYou almost immediately see a horde of giant spiders, they look stronger than usual.");
                 System.Console.WriteLine("You decide to try fighting them.");
                 battleBridge();
 
@@ -25,7 +25,7 @@ namespace MiniProject
             {
                 System.Console.WriteLine("You chose to follow the adventurers advice!\nAfter walking for a while you feel a cold chill in your spine.");
                 System.Console.WriteLine("You look around to see nothing and sprint away in fear.\n");
-                System.Console.WriteLine("Suddenly a horde of spiders appear infront of you. They look stronger than usual.");
+                System.Console.WriteLine("Suddenly a horde of giant spiders appear infront of you. They look stronger than usual.");
                 System.Console.WriteLine("You don't have any other choice then to fight.");
                 battleBridge();
 
@@ -39,7 +39,7 @@ namespace MiniProject
             secondBattle.StartBattle();
             Battle thirdBattle = new Battle(Player, Player.CurrentLocation.MonsterLivingHere);
             thirdBattle.StartBattle();
-            Console.WriteLine("After killing the spiders, you decide to return to the Bridge.\n There you see one of the guards from earlier.");
+            Console.WriteLine("After killing the giant spiders, you decide to return to the Bridge.\n There you see one of the guards from earlier.");
             Console.WriteLine("He waves at you and asks how it went, you show him the spider silk and he lets out a sigh of relief.\nAfter thanking you he gives you the Winner's Medal.");
             Player.Inventory.AddItem(World.ItemByID(World.ITEM_ID_WINNERS_MEDAL));
             Console.WriteLine("After bringing peace to the village your quest finally ends. Good job!\n THE END!");
